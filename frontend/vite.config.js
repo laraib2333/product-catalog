9dce272c767ab30c3e8df32bd178e46ae486a517
Laraib Ahmed <laraibahmed2333@gmail.com>
2025-07-12 12:09:44 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-
-// Use the imported defineConfig
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-      react(),
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', 
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path 
-      }
-    }
-  }
+  plugins: [react(), tailwindcss()],
 })
